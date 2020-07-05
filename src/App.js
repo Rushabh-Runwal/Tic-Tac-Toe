4782043818357b20a332b66226708aa3656961df
Rushabh-Runwal <65080982+Rushabh-Runwal@users.noreply.github.com>
2020-07-05 20:18:12 +0530

    Add theme

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import "react-toastify/dist/ReactToastify.css";
 //  getting bootstrap
 import { Card, CardBody, Container, Row, Col, Button } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.css";
-
+import ThemeTogglerButton from "./components/ThemeTogglerButton";
+import ThemeContext from "./Context/ThemeContext";
 import "./App.css";
+import HeroSection from "./components/heroSection";
 const itemArray = new Array(9).fill("empty");
 
 const App = () => {
@@ -15,51 +17,88 @@ const App = () => {
   const [isCross, setisCross] = useState(false);
 
   const checkWinner = () => {
-    if(itemArray[0] !== "empty" && itemArray[0]===itemArray[1] && itemArray[1]===itemArray[2]){
+    if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[1] &&
+      itemArray[1] === itemArray[2]
+    ) {
       setWinMessage(`${itemArray[0]} won`);
-    } else  if(itemArray[0] !== "empty" && itemArray[0]===itemArray[3] && itemArray[3]===itemArray[6]){
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[3] &&
+      itemArray[3] === itemArray[6]
+    ) {
       setWinMessage(`${itemArray[0]} won`);
-    } else  if(itemArray[0] !== "empty" && itemArray[0]===itemArray[4] && itemArray[4]===itemArray[8]){
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[4] &&
+      itemArray[4] === itemArray[8]
+    ) {
       setWinMessage(`${itemArray[0]} won`);
-    } else  if(itemArray[8] !== "empty" && itemArray[8]===itemArray[5] && itemArray[2]===itemArray[8]){
+    } else if (
+      itemArray[8] !== "empty" &&
+      itemArray[8] === itemArray[5] &&
+      itemArray[2] === itemArray[8]
+    ) {
       setWinMessage(`${itemArray[8]} won`);
-    } else  if(itemArray[8] !== "empty" && itemArray[7]===itemArray[6] && itemArray[7]===itemArray[8]){
+    } else if (
+      itemArray[8] !== "empty" &&
+      itemArray[7] === itemArray[6] &&
+      itemArray[7] === itemArray[8]
+    ) {
       setWinMessage(`${itemArray[8]} won`);
-    } else   if(itemArray[6] !== "empty" && itemArray[4]===itemArray[6] && itemArray[2]===itemArray[4]){
-      setWinMessage(`${itemArray[6]} won`);  
-    } else   if(itemArray[3] !== "empty" && itemArray[4]===itemArray[3] && itemArray[5]===itemArray[4]){
-      setWinMessage(`${itemArray[3]} won`); 
-    } else   if(itemArray[1] !== "empty" && itemArray[4]===itemArray[1] && itemArray[7]===itemArray[4]){
-      setWinMessage(`${itemArray[1]} won`);  
-    } else   if(!itemArray.includes("empty")){
-      setWinMessage(`It is a Draw`);
+    } else if (
+      itemArray[6] !== "empty" &&
+      itemArray[4] === itemArray[6] &&
+      itemArray[2] === itemArray[4]
+    ) {
+      setWinMessage(`${itemArray[6]} won`);
+    } else if (
+      itemArray[3] !== "empty" &&
+      itemArray[4] === itemArray[3] &&
+      itemArray[5] === itemArray[4]
+    ) {
+      setWinMessage(`${itemArray[3]} won`);
+    } else if (
+      itemArray[1] !== "empty" &&
+      itemArray[4] === itemArray[1] &&
+      itemArray[7] === itemArray[4]
+    ) {
+      setWinMessage(`${itemArray[1]} won`);
+    } else if (!itemArray.includes("empty")) {
+      setWinMessage(`It's' a Draw`);
     }
-  }  
+  };
 
   const reloadGame = () => {
     setisCross(false);
     setWinMessage("");
-    itemArray.fill("empty",0,9);
+    itemArray.fill("empty", 0, 9);
   };
 
   const changeItem = (itemNo) => {
-    if(winMessage){
-      return toast(winMessage, {type: "success"});
+    if (winMessage) {
+      return toast(winMessage, { type: "success" });
     }
 
-    if(itemArray[itemNo] === "empty"){
-      itemArray[itemNo] = isCross ? "cross" : "circle";  
-      
+    if (itemArray[itemNo] === "empty") {
+      itemArray[itemNo] = isCross ? "cross" : "circle";
+
       setisCross(!isCross);
     } else {
-      return toast("Already Filled" ,{typr: "error"});
+      return toast("Already Filled", { typr: "error" });
     }
 
     checkWinner();
   };
+  const themeHook = useState("light");
 
   return (
     <Container classNmae="p-5">
+      <ThemeContext.Provider value={themeHook}>
+        <ThemeTogglerButton />
+        <HeroSection />
+      </ThemeContext.Provider>
       <ToastContainer position="bottom-center" />
       <Row>
         <Col md={6} className="offset-md-3">
@@ -79,7 +118,12 @@ const App = () => {
           )}
           <div className="grid">
             {itemArray.map((item, index) => (
-              <Card color="warning" onClick={() => {changeItem(index)}}>
+              <Card
+                color="warning"
+                onClick={() => {
+                  changeItem(index);
+                }}
+              >
                 <CardBody className="box">
                   <Icon name={item} />
                 </CardBody>
